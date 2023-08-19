d6e16df09574650ab02fec000e880aa9d423533a
shimpux3 <shimpux3@gmail.com>
2023-08-19 06:58:40 +0100

    again

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,13 +31,7 @@ class HomeController extends Controller
         $email = $request->email;
         $password = $request->password;
 
-        // dd($email, $password);
-//        $user = User::where('email', $email)->first();
-//        if (password_verify($password, $user->password)) {
-//            return redirect('home');
-//        } else {
-//            return redirect('login');
-//        }
+
 
         if (Auth::attempt(['email' => $email, 'password' => $password])) {
             return redirect('home');
